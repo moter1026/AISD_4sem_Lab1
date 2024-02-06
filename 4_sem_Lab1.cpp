70065897b10b213beeabd933e1a3cef1af6b05da
moter1026 <matvej.sidoroff2016@yandex.ru>
2024-02-06 21:58:23 +0400

    first markup

---
+++ 4_sem_Lab1.cpp
@@ -0,0 +1,12 @@
+﻿// 4_sem_Lab1.cpp: определяет точку входа для приложения.
+//
+
+#include "4_sem_Lab1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ 4_sem_Lab1.h
@@ -0,0 +1,28 @@
+﻿// 4_sem_Lab1.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
+
+
+template<typename T>
+class Set_analog {
+private:
+	T _key;
+	T* _next_el;
+	T* _prev_el;
+public:
+	Set_analog() = default;
+	Set_analog(Set_analog& const obj) {
+
+	}
+	~Set_analog() {
+
+	}
+	get_key() { return this->_key; }
+	get_next() { return this->_next_el; }
+	get_prev() { return this->_prev_el; }
+};
