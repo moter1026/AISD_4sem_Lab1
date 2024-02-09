37af32b13f95a282187ffcf89ecf667f26908fb3
moter1026 <matvej.sidoroff2016@yandex.ru>
2024-02-09 23:31:14 +0400

    Практически готовая лаба. Нужно разобраться с замером времени

--- 4_sem_Lab1.cpp
+++ 4_sem_Lab1.cpp
@@ -4,7 +4,8 @@
 #include "4_sem_Lab1.h"
 #include <ctime>
 #include <random>
-
+#include <algorithm>
+#include <complex>
 
 using namespace std;
 
@@ -18,106 +19,232 @@ size_t lcg() {
 int main()
 {
     setlocale(LC_ALL, "ru");
-    random_device rd;   
-    mt19937 gen(rd());  
-    uniform_int_distribution<> dist(0, 150000);
-
-
-    std::cout << "Измеряем время для дерева поиска...\n";
-    int size[] = {1000, 10000, 100000};
-    double medium_insert[3];
-    double medium_contains[3];
-    double medium_add[3];
-    double medium_delete[3];
-    for (size_t s = 0; s < 3; ++s)
     {
-        std::cout << "\n>>> Выполняем процедуры для дерева размером " << size[s] <<"\n\n";
+        random_device rd;
+        mt19937 gen(rd());  
+        uniform_int_distribution<> dist(0, 300);
+    
+        std::vector<int> _vector;
+        _vector.push_back(dist(gen));
+        for (size_t i = 1; i < 500; ++i) {
+            _vector.push_back(dist(gen));
+        }
+
+        std::vector<int> repeat = repeat_els(&_vector);
+
+        for (size_t i = 0; i < repeat.size(); i++)
+        {
+            std::cout << repeat[i] << " ";
+        }
+        std::cout << "\n\n";
+    }
+
+    {
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> dist(0, 150000);
+
+        std::cout << "Измеряем время для дерева поиска...\n";
+        int size[] = { 1000, 10000, 100000 };
+        double set_medium_insert[3];
+        double set_medium_contains[3];
+        double set_medium_add[3];
+        double set_medium_delete[3];
+
+        double vector_medium_insert[3];
+        double vector_medium_contains[3];
+        double vector_medium_add[3];
+        double vector_medium_delete[3];
+
         const int attempt = 100;
         const int attempt_contains_add_del = 1000;
-        size_t time_insert[attempt];
-        size_t time_contains[attempt];
-        size_t time_add[attempt];
-        size_t time_delete[attempt];
 
-
-        std::cout << "Заполняем " << size[s] << " псевдо случайными числами...\n";
-        size_t sum_insert = 0;
-        for (size_t j = 0; j < attempt; ++j)
+        for (size_t s = 0; s < 3; ++s)
         {
-            int start_insert = std::clock();
+            std::cout << "\n>>> Выполняем процедуры для дерева размером " << size[s] << "\n\n";
+
+            std::cout << "Заполняем " << size[s] << " псевдо случайными числами...\n";
+            size_t sum_insert = 0;
+            for (size_t j = 0; j < attempt; ++j)
+            {
+                int start_insert = std::clock();
+                My_set<int> _set(static_cast<int>(lcg()));
+                for (size_t i = 1; i < size[s]; ++i) {
+                    _set.insert(static_cast<int>(lcg()));
+                }
+                //std::cout << j;
+                int end_insert = std::clock();
+                sum_insert += (end_insert - start_insert);
+            }
+            set_medium_insert[s] = (double)sum_insert / attempt;
+            std::cout << "Закончили заполнять. Сред. время - " << set_medium_insert[s] << "\n\n\n";
+
+
             My_set<int> _set(static_cast<int>(lcg()));
             for (size_t i = 1; i < size[s]; ++i) {
                 _set.insert(static_cast<int>(lcg()));
             }
-            //std::cout << j;
-            int end_insert = std::clock();
-            sum_insert += (end_insert - start_insert) / CLOCKS_PER_SEC;
-        }
-        medium_insert[s] = (double)sum_insert / attempt;
-        std::cout << "Закончили заполнять. Сред. время - " << medium_insert[s] << "\n\n\n";
+
+            std::cout << "Проверяем наличие значений...\n";
+            size_t sum_contains = 0;
+            for (size_t i = 0; i < attempt_contains_add_del; i++)
+            {
+                int start_contains = std::clock();
+
+                bool res = _set.contains(dist(gen));
+
+                int end_contains = std::clock();
+                sum_contains += (end_contains - start_contains);
+            }
+            set_medium_contains[s] = (double)sum_contains / attempt_contains_add_del;
+            std::cout << "Закончили проверять. Сред. время - " << set_medium_contains[s] << "\n\n\n";
 
 
-        My_set<int> _set(static_cast<int>(lcg()));
-        for (size_t i = 1; i < size[s]; ++i) {
-            _set.insert(static_cast<int>(lcg()));
+            std::cout << "Добавляем значения...\n";
+            size_t sum_add = 0;
+            for (size_t i = 0; i < attempt_contains_add_del; i++)
+            {
+                int start_add = std::clock();
+
+                _set.insert(dist(gen));
+
+int end_add = std::clock();
+sum_add += (end_add - start_add);
+            }
+            set_medium_add[s] = (double)sum_add / attempt_contains_add_del;
+            std::cout << "Закончили добавлять. Сред. время - " << set_medium_add[s] << "\n\n\n";
+
+            std::cout << "Удаляем значения...\n";
+            size_t sum_delete = 0;
+            for (size_t i = 0; i < attempt_contains_add_del; i++)
+            {
+                int start_delete = std::clock();
+
+                bool res = _set.erase(dist(gen));
+                int end_delete = std::clock();
+                sum_delete += (end_delete - start_delete);
+            }
+            set_medium_delete[s] = (double)sum_delete / attempt_contains_add_del;
+            std::cout << "Закончили удалять. Время - " << set_medium_delete[s] << "\n\n\n";
         }
-        
-        std::cout << "Проверяем наличие значений...\n";
-        size_t sum_contains = 0;
-        for (size_t i = 0; i < attempt_contains_add_del; i++)
+
+
+
+
+
+        for (size_t s = 0; s < 3; ++s)
         {
-            int start_contains = std::clock();
+            std::cout << "\n>>> Выполняем процедуры для VECTOR размером " << size[s] << "\n\n";
 
-            bool res = _set.contains(dist(gen));
+            std::cout << "Заполняем " << size[s] << " псевдо случайными числами...\n";
+            size_t sum_insert = 0;
+            for (size_t j = 0; j < attempt; ++j)
+            {
+                int start_insert = std::clock();
+                std::vector<int> _vector(static_cast<int>(lcg()));
+                for (size_t i = 1; i < size[s]; ++i) {
 
-            int end_contains = std::clock();
-            sum_contains += (end_contains - start_contains) / CLOCKS_PER_SEC;
+                    int val = static_cast<int>(lcg());
+                    for (size_t i = 0; i < _vector.size() - 1; i++)
+                    {
+                        if (val <= _vector[i])
+                        {
+                            _vector.insert(_vector.begin() + i, val);
+                            break;
+                        }
+                        else if (i == _vector.size() - 1)
+                        {
+                            _vector.push_back(val);
+                        }
+
+                    }
+                }
+                //std::cout << j;
+                int end_insert = std::clock();
+                sum_insert += (end_insert - start_insert);
+            }
+            vector_medium_insert[s] = (double)sum_insert / attempt;
+            std::cout << "Закончили заполнять. Сред. время - " << vector_medium_insert[s] << "\n\n\n";
+
+
+            std::vector<int> _vector(static_cast<int>(lcg()));
+            for (size_t i = 1; i < size[s]; ++i) {
+                _vector.push_back(static_cast<int>(lcg()));
+            }
+
+            std::cout << "Проверяем наличие значений...\n";
+            size_t sum_contains = 0;
+            for (size_t i = 0; i < attempt_contains_add_del; i++)
+            {
+                int start_contains = std::clock();
+
+                bool res = std::find(_vector.begin(), _vector.end(), dist(gen)) != _vector.end();
+
+                int end_contains = std::clock();
+                sum_contains += (end_contains - start_contains);
+            }
+            vector_medium_contains[s] = (double)sum_contains / attempt_contains_add_del;
+            std::cout << "Закончили проверять. Сред. время - " << vector_medium_contains[s] << "\n\n\n";
+
+
+            std::cout << "Добавляем значения...\n";
+            size_t sum_add = 0;
+            for (size_t i = 0; i < attempt_contains_add_del; i++)
+            {
+                int start_add = std::clock();
+
+                _vector.push_back(dist(gen));
+
+                int end_add = std::clock();
+                sum_add += (end_add - start_add);
+            }
+            vector_medium_add[s] = (double)sum_add / attempt_contains_add_del;
+            std::cout << "Закончили добавлять. Сред. время - " << vector_medium_add[s] << "\n\n\n";
+
+            std::cout << "Удаляем значения...\n";
+            size_t sum_delete = 0;
+            for (size_t i = 0; i < attempt_contains_add_del; i++)
+            {
+                int start_delete = std::clock();
+
+                auto res = std::find(_vector.begin(), _vector.end(), dist(gen));
+                if (res == _vector.end()) { continue; }
+                _vector.erase(res);
+
+                int end_delete = std::clock();
+                sum_delete += (end_delete - start_delete);
+            }
+            vector_medium_delete[s] = (double)sum_delete / attempt_contains_add_del;
+            std::cout << "Закончили удалять. Время - " << vector_medium_delete[s] << "\n\n\n";
         }
-        medium_contains[s] = sum_contains / attempt_contains_add_del;
-        std::cout << "Закончили проверять. Сред. время - " << medium_contains[s] << "\n\n\n";
+    }
 
 
-        std::cout << "Добавляем значения...\n";
-        size_t sum_add = 0;
-        for (size_t i = 0; i < attempt_contains_add_del; i++)
+
+    
+
+    
+    {
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> dist(0, 1000);
+
+        std::complex<float> cpl_float(10.5, 2.5);
+        My_set<std::complex<float>> _set(cpl_float);
+
+        std::cout << _set.get_element()->get_key() << "\n";
+
+        for (size_t i = 0; i < 1100; i++)
         {
-            int start_add = std::clock();
-
-            _set.insert(dist(gen));
-
-            int end_add = std::clock();
-            sum_add += (end_add - start_add) / CLOCKS_PER_SEC;
+            std::complex<float> cpl((float)dist(gen) / 100, (float)dist(gen) / 100);
+            _set.insert(cpl);
         }
-        medium_add[s] = sum_add / attempt_contains_add_del;
-        std::cout << "Закончили добавлять. Сред. время - " << medium_add[s] << "\n\n\n";
+        std::complex<float> cpl(1.93, 0.6);
+        _set.insert(cpl);
 
-        std::cout << "Удаляем значения...\n";
-        size_t sum_delete = 0;
-        for (size_t i = 0; i < attempt_contains_add_del; i++)
-        {
-            if (i == 100) {
-                int b = 0;
-            }
-            if (i == 200) {
-                int b = 0;
-            }
-            if (i == 300) {
-                int b = 0;
-            }
-            if (i == 400) {
-                int b = 0;
-            }
-            if (i == 500) {
-                int b = 0;
-            }
-            int start_delete = std::clock();
+        (_set.contains(cpl) == 0) ? std::cout << "false" << "\n" : std::cout << "true" << "\n";
 
-            bool res = _set.erase(dist(gen));
-            int end_delete = std::clock();
-            sum_delete += (end_delete - start_delete) / CLOCKS_PER_SEC;
-        }
-        medium_delete[s] = sum_delete / attempt_contains_add_del;
-        std::cout << "Закончили удалять. Время - " << medium_delete[s] << "\n\n\n";
+        std::cout << _set.erase(cpl_float) << "\n";
     }
     
     return 0;
--- 4_sem_Lab1.h
+++ 4_sem_Lab1.h
@@ -7,6 +7,8 @@
 #include <stdexcept>
 #include <vector>
 #include <stack>
+#include <algorithm>
+#include <complex>
 
 
 // TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
@@ -33,8 +35,52 @@ public:
 	T get_key() { return this->_key; }
 	Node* get_right() { return this->_right_ptr; }
 	Node* get_left() { return this->_left_ptr; }
+	void set_key(T key) { this->_key = key; }
 	void set_left(Node* left) { this->_left_ptr = left; }
 	void set_right(Node* right) { this->_right_ptr = right; }
+	void print() {
+		// Обход ЛКП - левый корень правый
+		if (this->get_left() != nullptr) {	// обходим левые ветки, спускаемся к самому маленькому эл-ту
+			this->get_left()->print();
+		}
+
+		std::cout << this->get_key() << " ";		// выводим корень
+
+		if (this->get_right() != nullptr)	// обходим правые ветки
+		{
+			this->get_right()->print();
+			return;
+		}
+		else
+		{
+			return;
+		}
+	}
+};
+template<typename T>
+class Node<std::complex<T>> {
+private:
+	std::complex<T> _key;
+	Node< std::complex<T>>* _right_ptr;
+	Node< std::complex<T>>* _left_ptr;
+	//List* _root_ptr;
+public:
+	Node() = default;
+	Node(Node< std::complex<T>>& const obj) {
+		this->_key = obj.get_key();
+		this->_left_ptr = nullptr;
+		this->_right_ptr = nullptr;
+	}
+	Node(std::complex<T> key) : _key(key), _right_ptr(nullptr), _left_ptr(nullptr) {}
+	Node(std::complex<T> key, Node< std::complex<T>>* right, Node< std::complex<T>>* left) : _key(key), _right_ptr(right), _left_ptr(left) {}
+	~Node() = default;
+
+	std::complex<T> get_key() { return this->_key; }
+	Node< std::complex<T>>* get_right() { return this->_right_ptr; }
+	Node< std::complex<T>>* get_left() { return this->_left_ptr; }
+	void set_key(std::complex<T> key) { this->_key = key; }
+	void set_left(Node< std::complex<T>>* left) { this->_left_ptr = left; }
+	void set_right(Node< std::complex<T>>* right) { this->_right_ptr = right; }
 	void print() {
 		// Обход ЛКП - левый корень правый
 		if (this->get_left() != nullptr) {	// обходим левые ветки, спускаемся к самому маленькому эл-ту
@@ -145,7 +191,6 @@ public:
 			}
 
 			delete current;
-			this->_count--;
 		}
 	}
 
@@ -229,13 +274,360 @@ public:
 				return false;
 			}
 			else if (key == el->get_key()) {
-				stk.pop();
-				delete el;
-				el = stk.top();
-				el->set_left(nullptr);
-				return true;
+				if (el->get_left() != nullptr && el->get_right() != nullptr)
+				{
+					Node<T>* now = el;
+					el = el->get_left();
+					stk.push(el);
+					while (el->get_right() != nullptr)
+					{
+						el = el->get_right();
+						stk.push(el);
+					}
+					now->set_key(el->get_key());
+				}
+				
+				if (el->get_left() != nullptr && el->get_right() == nullptr)
+				{
+					stk.pop();
+					Node<T>* root = stk.top();
+					if (root->get_left() == el) {
+						root->set_left(el->get_left());
+					}
+					else if(root->get_right() == el)
+					{
+						root->set_right(el->get_left());
+					}
+					this->_count--;
+					delete el;
+					return true;
+				}
+				else if (el->get_left() == nullptr && el->get_right() != nullptr)
+				{
+					stk.pop();
+					Node<T>* root = stk.top();
+					if (root->get_left() == el) {
+						root->set_left(el->get_right());
+					}
+					else if (root->get_right() == el)
+					{
+						root->set_right(el->get_right());
+					}
+					this->_count--;
+					delete el;
+					return true;
+				}
+				else
+				{
+					stk.pop();
+					Node<T>* root = stk.top();
+					if (root->get_left() == el) {
+						root->set_left(nullptr);
+					}
+					else if (root->get_right() == el)
+					{
+						root->set_right(nullptr);
+					}
+					this->_count--;
+					delete el;
+					return true;
+				}
 			}
 		}
 		return false;
 	}
+	
 };
+
+template<typename T>
+class My_set<std::complex<T>>  {
+	Node<std::complex<T>>* _element;
+	int _count;
+	double length(std::complex<T>* cmp) {
+		return sqrt(powl(cmp->real(), 2) + powl(cmp->imag(), 2));
+	}
+	bool areEqual(double one, double two, double epsilon = 0.00001) {
+		return std::abs(one - two) < epsilon;
+	}
+public:
+	My_set() : _element(new Node<std::complex<T>>(0, 0)), _count(1) {};
+	My_set(std::complex<T> key) : _count(1) {
+		this->_element = new Node<std::complex<T>>(key);
+	}
+	My_set(Node<std::complex<T>>* el) : _count(1) {
+		this->_element = new Node<std::complex<T>>(*el);
+	};
+	My_set(My_set<std::complex<T>>& const obj) {
+		if (!&obj) { throw std::runtime_error("Передан пустой объект"); }
+
+		Node<std::complex<T>>* el = obj.get_element(); // создаю один Node, чтобы у первого элемента был адрес и ключ
+		this->_element = new Node<std::complex<T>>(el->get_key());
+		std::stack<Node<std::complex<T>>*> stk;
+		while (el != nullptr || !stk.empty())
+		{
+			if (!stk.empty())
+			{
+				el = stk.top();
+				stk.pop();
+				if (el->get_right() != nullptr)
+				{
+					el = el->get_right();
+				}
+				else
+				{
+					el = nullptr;
+				}
+			}
+			while (el != nullptr)
+			{
+				stk.push(el);
+				this->insert(el->get_key());
+				this->_count++;
+				el = el->get_left();
+			}
+		}
+	}
+	My_set(Node<std::complex<T>>& const node) {
+		if (!&node) { throw std::runtime_error("Передан пустой объект"); }
+
+		Node<std::complex<T>>* el = &node;
+		this->_element = new Node<std::complex<T>>(el->get_key());
+		std::stack<Node<std::complex<T>>*> stk;
+		while (el != nullptr || !stk.empty())
+		{
+			if (!stk.empty())
+			{
+				el = stk.top();
+				stk.pop();
+				if (el->get_right() != nullptr)
+				{
+					el = el->get_right();
+				}
+				else
+				{
+					el = nullptr;
+				}
+			}
+			while (el != nullptr)
+			{
+				stk.push(el);
+				this->insert(el->get_key());
+				this->_count++;
+				el = el->get_left();
+			}
+		}
+	}
+	~My_set() {
+		std::stack<Node<std::complex<T>>*> stack;
+		stack.push(_element);
+
+		while (!stack.empty()) {
+			Node<std::complex<T>>* current = stack.top();
+			stack.pop();
+
+			if (current->get_right() != nullptr) {
+				stack.push(current->get_right());
+			}
+
+			if (current->get_left() != nullptr) {
+				stack.push(current->get_left());
+			}
+
+			delete current;
+		}
+	}
+
+
+	Node<std::complex<T>>* get_element() { return this->_element; }
+	bool insert(std::complex<T> key) {
+		Node<std::complex<T>>* el = this->_element;
+
+
+		double key_length = this->length(&key);
+
+		while (true) {
+			std::complex<T> el_cmp = el->get_key();
+			double el_length = this->length(&el_cmp);
+			if (areEqual(key_length, el_length))
+			{
+				break;
+			}
+			else if (key_length < el_length && el->get_left() != nullptr)
+			{
+				el = el->get_left();
+			}
+			else if (key_length < el_length && el->get_left() == nullptr) {
+				el->set_left(new Node(key));
+				this->_count++;
+				return true;
+			}
+			else if (key_length > el_length && el->get_right() != nullptr)
+			{
+				el = el->get_right();
+			}
+			else if (key_length > el_length && el->get_right() == nullptr) {
+				el->set_right(new Node(key));
+				this->_count++;
+				return true;
+			}
+			else {
+				break;
+			}
+		}
+		return false;
+	}
+	bool contains(std::complex<T> key) {
+		Node<std::complex<T>>* el = this->_element;
+		if (!el) return false;
+		
+
+		double key_length = this->length(&key);
+
+		while (true) {
+			std::complex<T> el_cmp = el->get_key();
+			double el_length = this->length(&el_cmp);
+
+			if (areEqual(key_length, el_length)) {
+				return true;
+			}
+			else if (key_length < el_length && el->get_left() != nullptr)
+			{
+				el = el->get_left();
+			}
+			else if (key_length < el_length && el->get_left() == nullptr) {
+				return false;
+			}
+			else if (key_length > el_length && el->get_right() != nullptr)
+			{
+				el = el->get_right();
+			}
+			else if (key_length > el_length && el->get_right() == nullptr) {
+				return false;
+			}
+			 
+		}
+		return false;
+	}
+	bool erase(std::complex<T> key) {
+		Node<std::complex<T>>* el = this->_element;
+		if (el == nullptr) return false;
+		std::stack<Node<std::complex<T>>*> stk;
+		stk.push(el);
+
+
+		double key_length = this->length(&key);
+
+		while (true) {
+			std::complex<T> el_cmp = el->get_key();
+			double el_length = this->length(&el_cmp);
+
+			if (areEqual(key_length, el_length)) {
+				if (el->get_left() != nullptr && el->get_right() != nullptr)
+				{
+					Node<std::complex<T>>* now = el;
+					el = el->get_left();
+					stk.push(el);
+					while (el->get_right() != nullptr)
+					{
+						el = el->get_right();
+						stk.push(el);
+					}
+					now->set_key(el->get_key());
+				}
+
+				if (el->get_left() != nullptr && el->get_right() == nullptr)
+				{
+					stk.pop();
+					Node<std::complex<T>>* root = stk.top();
+					if (root->get_left() == el) {
+						root->set_left(el->get_left());
+					}
+					else if (root->get_right() == el)
+					{
+						root->set_right(el->get_left());
+					}
+					this->_count--;
+					delete el;
+					return true;
+				}
+				else if (el->get_left() == nullptr && el->get_right() != nullptr)
+				{
+					stk.pop();
+					Node<std::complex<T>>* root = stk.top();
+					if (root->get_left() == el) {
+						root->set_left(el->get_right());
+					}
+					else if (root->get_right() == el)
+					{
+						root->set_right(el->get_right());
+					}
+					this->_count--;
+					delete el;
+					return true;
+				}
+				else
+				{
+					stk.pop();
+					Node<std::complex<T>>* root = stk.top();
+					if (root->get_left() == el) {
+						root->set_left(nullptr);
+					}
+					else if (root->get_right() == el)
+					{
+						root->set_right(nullptr);
+					}
+					this->_count--;
+					delete el;
+					return true;
+				}
+			}
+			else if (key_length < el_length && el->get_left() != nullptr)
+			{
+				el = el->get_left();
+				stk.push(el);
+			}
+			else if (key_length < el_length && el->get_left() == nullptr) {
+				return false;
+			}
+			else if (key_length > el_length && el->get_right() != nullptr)
+			{
+				el = el->get_right();
+				stk.push(el);
+			}
+			else if (key_length > el_length && el->get_right() == nullptr) {
+				return false;
+			}
+			 
+		}
+		return false;
+	}
+	void print() {
+		this->_element->print();
+	}
+};
+
+
+std::vector<int> repeat_els(std::vector<int>* _vec) {
+	std::vector<int> sort = *_vec;
+	std::sort(sort.begin(), sort.end());
+	
+	std::vector<int> new_vector;
+	bool add = false;
+	for (size_t i = 0; i < sort.size() - 2; i++)
+	{
+		if (sort[i] == sort[i + 1] && add == false)
+		{
+			new_vector.push_back(sort[i]);
+			add = true;
+			continue;
+		}
+		else if (sort[i] == sort[i + 1] && add != false) {
+			continue;
+		}
+		else {
+			add = false;
+		}
+	}
+
+	return new_vector;
+}
